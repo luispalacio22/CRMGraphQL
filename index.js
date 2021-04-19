9d79f9133245e2efdf16b00cd2b35d22713f5795
Luis Palacio <luispalacio11c@gmail.com>
2021-04-19 00:39:27 -0500

    commit

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-const {ApolloServer}=require('apollo-server');
-const typeDefs=require('./db/schema');
-const resolvers=require('./db/resolvers');
-const conectarDB=require('./config/db');
-const jwt= require('jsonwebtoken');
-require('dotenv').config({path: 'variables.env'});
+const { ApolloServer  } = require('apollo-server');
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
+const conectarDB = require('./config/db');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
 
-//conectar a la base de datos 
+// Conectar a la base de datos
 conectarDB();
 
-//Servidor
-const server=new ApolloServer({
+// servidor
+const server = new ApolloServer({
     typeDefs,
-    resolvers,
-    context:({req})=>{
-        console.log(req.headers);
+    resolvers, 
+    context: ({req}) => {
+        // console.log(req.headers['authorization'])
 
-        const token=req.headers['authorization']||'';
-        console.log(token)
-        if(token){
+        // console.log(req.headers);
+
+        const token = req.headers['authorization'] || '';
+        if(token) {
             try {
-                const usuario=jwt.verify(token.replace('Bearer',''),process.env.SECRETA);
-                return{
+                const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.SECRETA );
+                // console.log(usuario);
+                return {
                     usuario
                 }
             } catch (error) {
                 console.log('Hubo un error');
                 console.log(error);
-                
             }
         }
     }
 });
 
-//Arrancar el servidor
-server.listen({port: process.env.PORT || 4000}).then(({url})=>{
+// arrancar el servidor
+server.listen({ port: process.env.PORT || 4000 }).then( ({url}) => {
     console.log(`Servidor listo en la URL ${url}`)
-})
+} )
